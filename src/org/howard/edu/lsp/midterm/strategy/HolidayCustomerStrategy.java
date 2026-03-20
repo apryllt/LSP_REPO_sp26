f1957d881dc6ffdf0f5908ef9add2c01e28ec3ad
apryllt <122575376+apryllt@users.noreply.github.com>
2026-03-19 20:41:10 -0400

    Refine Javadoc comments in HolidayCustomerStrategy
    Updated Javadoc comments for clarity.

--- src/org/howard/edu/lsp/midterm/strategy/HolidayCustomerStrategy.java
+++ src/org/howard/edu/lsp/midterm/strategy/HolidayCustomerStrategy.java
@@ -2,11 +2,15 @@ package org.howard.edu.lsp.midterm.strategy;
 
 /**
  * Strategy for holiday promotion (15% discount).
+ * @author Aprille Thomas
  */
 public class HolidayCustomerStrategy implements PriceStrategy {
 
     /**
-     * Applies a 15% discount for holiday promotion.
+     * Applies a 15% discount to the original price for holiday promotions.
+     *
+     * @param price The original price.
+     * @return The price after applying a 15% discount.
      */
     @Override
     public double calculate(double price) {
