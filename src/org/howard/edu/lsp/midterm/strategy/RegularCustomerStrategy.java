76b33f3b7392911edce3388eea5b52f04d6e9fd6
apryllt <122575376+apryllt@users.noreply.github.com>
2026-03-19 20:41:47 -0400

    Add author info and documentation comments
    Added author information to the RegularCustomerStrategy class and its method documentation.

--- src/org/howard/edu/lsp/midterm/strategy/RegularCustomerStrategy.java
+++ src/org/howard/edu/lsp/midterm/strategy/RegularCustomerStrategy.java
@@ -2,11 +2,15 @@ package org.howard.edu.lsp.midterm.strategy;
 
 /**
  * Strategy for regular customers (no discount).
+ * @author Aprille Thomas
  */
 public class RegularCustomerStrategy implements PriceStrategy {
 
     /**
      * Returns the price unchanged for regular customers.
+     *
+     * @param price The original price.
+     * @return The same price without any discount.
      */
     @Override
     public double calculate(double price) {
