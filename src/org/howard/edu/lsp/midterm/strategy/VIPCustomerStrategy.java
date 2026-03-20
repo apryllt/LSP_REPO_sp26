262eaa970fdb1bdab1ae277bae3a619faf6eb60c
apryllt <122575376+apryllt@users.noreply.github.com>
2026-03-19 20:42:58 -0400

    Clarify discount application in VIPCustomerStrategy
    Updated method documentation to clarify discount application.

--- src/org/howard/edu/lsp/midterm/strategy/VIPCustomerStrategy.java
+++ src/org/howard/edu/lsp/midterm/strategy/VIPCustomerStrategy.java
@@ -2,11 +2,15 @@ package org.howard.edu.lsp.midterm.strategy;
 
 /**
  * Strategy for VIP customers (20% discount).
+ * @author Aprille Thomas
  */
 public class VIPCustomerStrategy implements PriceStrategy {
 
     /**
-     * Applies a 20% discount for VIP customers.
+     * Applies a 20% discount to the original price.
+     *
+     * @param price The original price.
+     * @return The price after applying a 20% discount.
      */
     @Override
     public double calculate(double price) {
