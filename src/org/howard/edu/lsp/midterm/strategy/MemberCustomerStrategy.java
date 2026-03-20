51cebd4e19f4459bccd5f7b645bc8fdfe6006400
apryllt <122575376+apryllt@users.noreply.github.com>
2026-03-19 20:42:24 -0400

    Clarify discount application in MemberCustomerStrategy
    Updated JavaDoc to clarify discount application.

--- src/org/howard/edu/lsp/midterm/strategy/MemberCustomerStrategy.java
+++ src/org/howard/edu/lsp/midterm/strategy/MemberCustomerStrategy.java
@@ -2,11 +2,15 @@ package org.howard.edu.lsp.midterm.strategy;
 
 /**
  * Strategy for members (10% discount).
+ *@author Aprille Thomas
  */
 public class MemberCustomerStrategy implements PriceStrategy {
 
     /**
-     * Applies a 10% discount for members.
+     * Applies a 10% discount to the original price.
+     *
+     * @param price The original price.
+     * @return The price after applying a 10% discount.
      */
     @Override
     public double calculate(double price) {
