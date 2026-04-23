2dc122b6577655357839cc96f48ce619dc580973
apryllt <122575376+apryllt@users.noreply.github.com>
2026-04-23 19:55:25 -0400

    Implement Driver class to generate reports

---
+++ src/org/howard/edu/lsp/finalexam/question2/Driver.java
@@ -0,0 +1,18 @@
+package org.howard.edu.lsp.finalexam.question2;
+
+import java.util.*;
+
+public class Driver {
+    public static void main(String[] args) {
+
+        List<Report> reports = new ArrayList<>();
+
+        reports.add(new StudentReport());
+        reports.add(new CourseReport());
+
+        for (Report r : reports) {
+            r.generateReport();
+            System.out.println();
+        }
+    }
+}
